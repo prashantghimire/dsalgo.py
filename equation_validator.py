90157deea8a1e5641e9766ba1bcc0a1a8ee828ac
Prashant Ghimire <prashant.ghimire@selu.edu>
2021-04-29 23:47:33 -0500

    equation_validator

--- equation_validator.py
+++ equation_validator.py
@@ -12,11 +12,16 @@ def validate(eqn: str):
             stack.append(c)
 
         if c in brackets_map.values():
+            if len(stack) == 0:
+                return False
             top = stack.pop()
             if c != brackets_map[top]:
                 return False
-    return True
+
+    return len(stack) == 0
 
 
+print(validate(']'))
 print(validate('{(a+b)}(a-b)'))
 print(validate('{(a+b})(a-b)'))
+print(validate('{(({'))
